699c2c56abed198e02fd8144d3919b548bab3b4a
Ali_Baheri <ali.baheri4614@gmail.com>
2017-12-27 11:28:51 -0500

    v4

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ from sklearn.model_selection import train_test_split
 
 steering_correction = .15
 steering_threshold = .25
-batch_size = 32
-num_epoch = 1
+batch_size = 40
+num_epoch = 2
 
 lines = []
 with open('data/driving_log.csv') as csvfile:
@@ -115,23 +115,23 @@ import csv
 import cv2
 
 def nvidia_model():
-	'''Nvidia architectural model with dropout in FCs.'''
 	model = Sequential()
-	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-	model.add(Cropping2D(cropping=((65,25), (0,0))))  # 70-top, 25-bottom
-	model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
-	model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
-	model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
-	model.add(Convolution2D(64,3,3, activation='relu'))
-	model.add(Convolution2D(64,3,3, activation='relu'))
+	model.add(Lambda(lambda x: x / 127.5 - 1, input_shape=(160,320,3)))
+	model.add(Cropping2D(cropping=((70,25), (0,0))))  # 70-top, 25-bottom
+	model.add(Convolution2D(24,5,5, subsample=(2,2), activation='elu'))
+	model.add(Convolution2D(36,5,5, subsample=(2,2), activation='elu'))
+	model.add(Convolution2D(48,5,5, subsample=(2,2), activation='elu'))
+	model.add(Convolution2D(64,3,3, activation='elu'))
+	model.add(Convolution2D(64,3,3, activation='elu'))
+	model.add((Droput(0.5))
 	model.add(Flatten())
-	model.add(Dense(100))
-	model.add(Dropout(0.5))
-	model.add(Dense(50))
-	model.add(Dropout(0.5))
-	model.add(Dense(20))
-	model.add(Dropout(0.5))
-	model.add(Dense(10))
+	model.add(Dense(100, activation='elu'))
+	#model.add(Dropout(0.5))
+	model.add(Dense(50, activation='elu'))
+	#model.add(Dropout(0.5))
+	#model.add(Dense(20))
+	#model.add(Dropout(0.5))
+	model.add(Dense(10, activation='elu'))
 	model.add(Dense(1))
 	model.summary()
 	return model
