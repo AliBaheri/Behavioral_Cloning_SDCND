c25a913cc5ae541b6141f29c6684157f90e53b7d
Ali_Baheri <ali.baheri4614@gmail.com>
2017-12-27 10:23:25 -0500

    v3

--- model.py
+++ model.py
@@ -1,164 +1,158 @@
-import pandas as pd # data analysis toolkit - create, read, update, delete datasets
-import numpy as np #matrix math
-from sklearn.model_selection import train_test_split #to split out training and testing data 
-#keras is a high level wrapper on top of tensorflow (machine learning library)
-#The Sequential container is a linear stack of layers
-from keras.models import Sequential
-#popular optimization strategy that uses gradient descent 
-from keras.optimizers import Adam
-#to save our model periodically as checkpoints for loading later
-from keras.callbacks import ModelCheckpoint
-#what types of layers do we want our model to have?
-from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
-#helper class to define input shape and generate training images given image paths & steering angles
-from utils import INPUT_SHAPE, batch_generator
-#for command line arguments
-import argparse
-#for reading files
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import cv2
+import csv
+import math
+from sklearn.model_selection import train_test_split
 
-#for debugging, allows for reproducible (deterministic) results 
-np.random.seed(0)
+steering_correction = .15
+steering_threshold = .25
+batch_size = 32
+num_epoch = 1
 
+lines = []
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
 
-def load_data(args):
-    """
-    Load training data and split it into training and validation set
-    """
-    #reads CSV file into a single dataframe variable
-    data_df = pd.read_csv('data/driving_log.csv', names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
+del(lines[0]) 
+train_set, valid_set = train_test_split(lines, test_size=0.2, random_state=100)
 
-    #yay dataframes, we can select rows and columns by their names
-    #we'll store the camera images as our input data
-    X = data_df[['center', 'left', 'right']].values
-    #and our steering commands as our output data
-    y = data_df['steering'].values
+    
+train_set, valid_set = train_test_split(lines, test_size = 0.2)
 
-    #now we can split the data into a training (80), testing(20), and validation set
-    #thanks scikit learn
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
-
-    return X_train, X_valid, y_train, y_valid
-
-
-def build_model(args):
-    """
-    NVIDIA model used
-    Image normalization to avoid saturation and make gradients work better.
-    Convolution: 5x5, filter: 24, strides: 2x2, activation: ELU
-    Convolution: 5x5, filter: 36, strides: 2x2, activation: ELU
-    Convolution: 5x5, filter: 48, strides: 2x2, activation: ELU
-    Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
-    Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
-    Drop out (0.5)
-    Fully connected: neurons: 100, activation: ELU
-    Fully connected: neurons: 50, activation: ELU
-    Fully connected: neurons: 10, activation: ELU
-    Fully connected: neurons: 1 (output)
-
-    # the convolution layers are meant to handle feature engineering
-    the fully connected layer for predicting the steering angle.
-    dropout avoids overfitting
-    ELU(Exponential linear unit) function takes care of the Vanishing gradient problem. 
-    """
-    model = Sequential()
-    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
-    model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(64, 3, 3, activation='elu'))
-    model.add(Conv2D(64, 3, 3, activation='elu'))
-    model.add(Dropout(args.keep_prob))
-    model.add(Flatten())
-    model.add(Dense(100, activation='elu'))
-    model.add(Dense(50, activation='elu'))
-    model.add(Dense(10, activation='elu'))
-    model.add(Dense(1))
-    model.summary()
-
-    return model
+def shadow_augmentation(image):
+	'''refer to: https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9
+	'''
+	height, width, _ = image.shape
+	imgtop_y = width*np.random.uniform() # 320
+	imgtop_x = 0
+	imgbot_y = width*np.random.uniform()
+	imgbot_x = height # 160
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+	shadow_mask = 0*image[:,:,1]
+	mask_X = np.mgrid[0:image.shape[0], 0:image.shape[1]][0]
+	mask_Y = np.mgrid[0:image.shape[0], 0:image.shape[1]][1]
+	shadow_mask[((mask_X - imgtop_x)*(imgbot_y - imgtop_y) - (imgbot_x - imgtop_x)*(mask_Y - imgtop_y) >=0)] = 1
+	if np.random.randint(2) == 1:
+		condition_1 = shadow_mask == 1
+		condition_0 = shadow_mask == 0
+		if np.random.randint(2) == 1:
+			image[:,:,1][condition_1] = image[:,:,1][condition_1]*.5 # random brightness
+		else:
+			image[:,:,1][condition_0] = image[:,:,1][condition_0]*.5 
+	image = cv2.cvtColor(image, cv2.COLOR_HLS2RGB)
+	return image 
 
 
-def train_model(model, args, X_train, X_valid, y_train, y_valid):
-    """
-    Train the model
-    """
-    #Saves the model after every epoch.
-    #quantity to monitor, verbosity i.e logging mode (0 or 1), 
-    #if save_best_only is true the latest best model according to the quantity monitored will not be overwritten.
-    #mode: one of {auto, min, max}. If save_best_only=True, the decision to overwrite the current save file is
-    # made based on either the maximization or the minimization of the monitored quantity. For val_acc, 
-    #this should be max, for val_loss this should be min, etc. In auto mode, the direction is automatically
-    # inferred from the name of the monitored quantity.
-    checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
-                                 monitor='val_loss',
-                                 verbose=0,
-                                 save_best_only=args.save_best_only,
-                                 mode='auto')
+def brightness_process(image):
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV) # convert from RGB to HSV color
+	brightness_random =  .23 + np.random.uniform() # random brightness 
+	image[:,:,2] = image[:,:,2]*brightness_random  # add brightness to the image
+	image[:,:,2][image[:,:,2]>255] = 255		   # keep image at 255 if greater than that 
+	image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB) # convert back to RGB from HSV
+	return image
 
-    #calculate the difference between expected steering angle and actual steering angle
-    #square the difference
-    #add up all those differences for as many data points as we have
-    #divide by the number of them
-    #that value is our mean squared error! this is what we want to minimize via
-    #gradient descent
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
+def process_images(center_path, left_path, right_path, steering, images, steering_set):
+	# center
+	center_image = cv2.imread('data/IMG/' + center_path.split('/')[-1]) # extract the image from the path
+	center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)		# convert BGR to RGB
+	center_image = brightness_process(center_image)						# brightness process
+	center_image = shadow_augmentation(center_image)					# shadow augmentation 
+	images.append(center_image)											
+	steering_set.append(steering)
+	# left
+	left_image = cv2.imread('data/IMG/' + left_path.split('/')[-1])
+	left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
+	left_image = brightness_process(left_image)
+	left_image = shadow_augmentation(left_image)
+	images.append(left_image)
+	steering_set.append(steering + steering_correction)
+	# right
+	right_image = cv2.imread('data/IMG/' + right_path.split('/')[-1])
+	right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
+	right_image = brightness_process(right_image)
+	right_image = shadow_augmentation(right_image)
+	images.append(right_image)
+	steering_set.append(steering - steering_correction)
+	if abs(steering) > steering_threshold:			# if steering is greater than steering_threshold, then flip the image.
+		images.append(cv2.flip(center_image, 1)) 	# flip images (augmentation)
+		steering_set.append(-steering)
+		images.append(cv2.flip(left_image, 1))		
+		steering_set.append(-(steering + steering_correction))
+		images.append(cv2.flip(right_image, 1))		
+		steering_set.append(-(steering - steering_correction))
 
-    #Fits the model on data generated batch-by-batch by a Python generator.
+def generators(datasets, batch_size=batch_size):
+	while True: 
+		shuffle(datasets) # shuffle the datasets
+		for offset in range(0, len(datasets), batch_size):
+			batch_lines = datasets[offset : offset + batch_size]
+			images = []
+			steering_set = []
+			for line in batch_lines:
+				steering = float(line[3]) 
+				process_images(line[0], line[1], line[2], steering, images, steering_set)
+			X_train = np.asarray(images)
+			y_train = np.asarray(steering_set)
+			yield shuffle(X_train, y_train, random_state=100)
+            
 
-    #The generator is run in parallel to the model, for efficiency. 
-    #For instance, this allows you to do real-time data augmentation on images on CPU in 
-    #parallel to training your model on GPU.
-    #so we reshape our data into their appropriate batches and train our model simulatenously
-    model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
-                        args.samples_per_epoch,
-                        args.nb_epoch,
-                        max_q_size=1,
-                        validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
-                        nb_val_samples=len(X_valid),
-                        callbacks=[checkpoint],
-                        verbose=1)
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+# keras libs
+from keras import backend
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers import Convolution2D, Cropping2D
+# misc. import
+import numpy as np 
+import csv
+import cv2
 
-#for command line args
-def s2b(s):
-    """
-    Converts a string to boolean value
-    """
-    s = s.lower()
-    return s == 'true' or s == 'yes' or s == 'y' or s == '1'
+def nvidia_model():
+	'''Nvidia architectural model with dropout in FCs.'''
+	model = Sequential()
+	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+	model.add(Cropping2D(cropping=((65,25), (0,0))))  # 70-top, 25-bottom
+	model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+	model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+	model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+	model.add(Convolution2D(64,3,3, activation='relu'))
+	model.add(Convolution2D(64,3,3, activation='relu'))
+	model.add(Flatten())
+	model.add(Dense(100))
+	model.add(Dropout(0.5))
+	model.add(Dense(50))
+	model.add(Dropout(0.5))
+	model.add(Dense(20))
+	model.add(Dropout(0.5))
+	model.add(Dense(10))
+	model.add(Dense(1))
+	model.summary()
+	return model
 
+def run_the_network():
+    # architectural network (nvidia-based)
+    model = nvidia_model()
+    # get data
+    #training_set, validation_set = get_log()
+    # train the model and fit generator
+    train_set_generator = generators(train_set, batch_size=batch_size)
+    valid_set_generator = generators(valid_set, batch_size=batch_size)
+    # params for fit generator
+    samples_per_epoch = (3*len(train_set)//batch_size)*batch_size
+    nb_val_samples = len(valid_set)
+    
+    model.compile(loss='mse', optimizer='adam')
+    model.fit_generator(train_set_generator, samples_per_epoch= samples_per_epoch, validation_data=valid_set_generator, nb_val_samples=nb_val_samples, nb_epoch=num_epoch, verbose=1)
 
-def main():
-    """
-    Load train/validation data set and train the model
-    """
-    parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
-    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
-    parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
-    parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
-    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
-    parser.add_argument('-s', help='samples per epoch',     dest='samples_per_epoch', type=int,   default=20000)
-    parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
-    parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=s2b,   default='true')
-    parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1.0e-4)
-    args = parser.parse_args()
+    model.save('model_ali.h5')
+    
+run_the_network()
 
-    #print parameters
-    print('-' * 30)
-    print('Parameters')
-    print('-' * 30)
-    for key, value in vars(args).items():
-        print('{:<20} := {}'.format(key, value))
-    print('-' * 30)
-
-    #load data
-    data = load_data(args)
-    #build model
-    model = build_model(args)
-    #train model on data, it saves as model.h5 
-    train_model(model, args, *data)
-
-
-if __name__ == '__main__':
-    main()
-
+backend.clear_session() 
